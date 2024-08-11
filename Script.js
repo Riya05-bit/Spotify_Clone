225aa4c9a1e4766c89698c4314e6ec10213cb4e3
Riya05-bit <136188578+Riya05-bit@users.noreply.github.com>
2024-08-11 11:33:42 +0530

    Update Script.js

--- Script.js
+++ Script.js
@@ -10,7 +10,7 @@ let masterSongName = document.getElementById('masterSongName');
 let songItems = Array.from(document.getElementsByClassName('songItem'));
 
 let songs = [
-    { songName: "Hass Hass", filePath: "1.mp3", coverPath: "covers/1.jpg" },
+    { songName: "Hass1 Hass", filePath: "1.mp3", coverPath: "covers/1.jpg" },
     { songName: "Greedy", filePath: "2.mp3", coverPath: "covers/2.jpg" },
     { songName: "Vampire", filePath: "3.mp3", coverPath: "covers/3.jpg" },
     { songName: "Water", filePath: "4.mp3", coverPath: "covers/4.jpg" },
